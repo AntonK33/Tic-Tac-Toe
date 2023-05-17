c7df7c5addae83b9edb13e5b829797029c3d78b7
Anton Kulakov <92113717+AntonK33@users.noreply.github.com>
2023-05-17 22:19:08 +0300

    игра работает нужно добавить отображение следующего игрока

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react';
 import css from './Board.module.css';
 
-const Square = onChange => {
-  return <button className={css.square_style} onClick={onChange}></button>;
+const Square = ({ onChange, value }) => {
+  return (
+    <button className={css.square_style} onClick={onChange}>
+      {value}
+    </button>
+  );
 };
 export const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xNext, setXNext] = useState(true);
+  console.log(squares);
 
   const hendlerClick = i => {
-    if (squares[i]) {
+    if (squares[i] || nextPlayer) {
       return;
     }
-    const nextSquares = squares.slice();
+    let nextSquares = squares;
     if (xNext) {
       nextSquares[i] = 'x';
     } else {
@@ -21,7 +26,18 @@ export const Board = () => {
 
     setSquares(nextSquares);
     setXNext(!xNext);
+    //console.log(squares);
+    calculateWinner(squares);
   };
+  let show;
+  let winner = nextPlayer;
+
+  if (winner) {
+    show = 'Winner: ' + winner;
+  } else {
+    show = 'Next player: ';
+  }
+
   return (
     <div
       style={{
@@ -31,6 +47,9 @@ export const Board = () => {
         alignItems: 'center',
       }}
     >
+      <div>
+        <p>{show}</p>
+      </div>
       <div>
         <Square
           value={squares[0]}
@@ -101,94 +120,49 @@ export const Board = () => {
       </div>
     </div>
   );
-  //return (
-  // (
-  //   <square
-  //     styleName={css.square_style}
-  //     value={squares[0]}
-  //     onChange={() => {
-  //       hendlerClick(0);
-  //     }}
-  //   />
-  // ),
-  // (
-  //   <square
-  //     value={squares[1]}
-  //     onChange={() => {
-  //       hendlerClick(1);
-  //     }}
-  //   />
-  // ),
-  // (
-  //   <square
-  //     value={squares[2]}
-  //     onChange={() => {
-  //       hendlerClick(2);
-  //     }}
-  //   />
-  // ),
-  // (
-  //   <square
-  //     value={squares[3]}
-  //     onChange={() => {
-  //       hendlerClick(3);
-  //     }}
-  //   />
-  // ),
-  // (
-  //   <square
-  //     value={squares[4]}
-  //     onChange={() => {
-  //       hendlerClick(4);
-  //     }}
-  //   />
-  // ),
-  // (
-  //   <square
-  //     value={squares[5]}
-  //     onChange={() => {
-  //       hendlerClick(5);
-  //     }}
-  //   />
-  // ),
-  // (
-  //   <square
-  //     value={squares[6]}
-  //     onChange={() => {
-  //       hendlerClick(6);
-  //     }}
-  //   />
-  // ),
-  // (
-  //   <square
-  //     value={squares[7]}
-  //     onChange={() => {
-  //       hendlerClick(7);
-  //     }}
-  //   />
-  // ),
-  // (
-  //   <square
-  //     value={squares[8]}
-  //     onChange={() => {
-  //       hendlerClick(8);
-  //     }}
-  //   />
-  // )
-  // );
 };
 
-// return (
-//   <div
-//     style={{
-//       height: '100vh',
-//       display: 'flex',
-//       justifyContent: 'center',
-//       alignItems: 'center',
-//       fontSize: 40,
-//       color: '#010101',
-//     }}
-//   >
-//     React homework template
-//   </div>
-// );
+let nextPlayer;
+
+const calculateWinner = squares => {
+  const line = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [8, 4, 0],
+    [6, 4, 2],
+    [2, 1, 0],
+    [5, 4, 3],
+    [8, 7, 6],
+    [6, 3, 0],
+    [7, 4, 1],
+    [8, 5, 2],
+  ];
+
+  // const lineMap = line.map(lin => lin);
+  // const [a, b, c, d] = lineMap;
+  // console.log(lineMap);
+  // console.log(a, b, c, d);
+  // if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+  //   return squares[a];
+  // } else {
+  //   return;
+  // }
+
+  for (let i = 0; i < line.length; i++) {
+    const [a, b, c] = line[i];
+    console.log(line[i]);
+    console.log(a, b, c);
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return (nextPlayer = squares[a]);
+    }
+    //else {
+    //     return;
+    //   }
+  }
+};
